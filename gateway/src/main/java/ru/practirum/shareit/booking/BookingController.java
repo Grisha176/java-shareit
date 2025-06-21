34dc0cfbb24377343cf61b1b2856b22f783891cf
Grigoriy <GrishaNaha@yandex.ru>
2025-06-21 14:37:10 +0300

    исправление тестов

--- gateway/src/main/java/ru/practirum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practirum/shareit/booking/BookingController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.practirum.shareit.booking.dto.BookingState;
 import ru.practirum.shareit.booking.dto.NewBookingRequest;
 
-import java.util.List;
-
 
 @Controller
 @RequiredArgsConstructor
